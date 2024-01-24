ddbca85595bf8d27b5adbdcc70d2c829a868eb3e
Natdanai <natdanai.thaiyala@gmail.com>
2024-01-24 17:23:51 +0700

    modules and packages

---
+++ 16-Python/lesson_14_modules_packages/myprogram.py
@@ -0,0 +1,16 @@
+import mymodule as mm
+mm.func_in_module()
+
+# ==
+# import mymodule
+# mymodule.func_in_module()
+
+# ==
+# from mymodule import func_in_module
+# func_in_module()
+
+# from mymodule import *
+# func_in_module
+
+# if run python myprogram.py
+# python generate __pycache__ folder complie byte code first
