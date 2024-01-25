42d5052cb396b63dfd871e49a37dab6374f822bc
Natdanai <natdanai.thaiyala@gmail.com>
2024-01-25 12:58:24 +0700

    Map between html page and project

--- 17-Django-Python/My_Django_Stuff/first_project/first_project/urls.py
+++ 17-Django-Python/My_Django_Stuff/first_project/first_project/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from first_app import views
 
 urlpatterns = [
+    #Map url and index html in first_app -> views.py
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+
 ]
